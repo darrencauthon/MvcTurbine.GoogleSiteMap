140399fc376e5b4b8ae094dc5071fa6b9f4cec09
darrencauthon <darren@cauthon.com>
2010-08-04 11:32:55 -0500

    Create sample app.

---
+++ src/MvcTurbine.GoogleSiteMap.SampleApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcTurbine.GoogleSiteMap.SampleApp.MvcApplication" Language="C#" %>
---
+++ src/MvcTurbine.GoogleSiteMap.SampleApp/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using MvcTurbine.ComponentModel;
+using MvcTurbine.Unity;
+using MvcTurbine.Web;
+
+namespace MvcTurbine.GoogleSiteMap.SampleApp
+{
+    public class MvcApplication : TurbineApplication
+    {
+        public MvcApplication()
+        {
+            ServiceLocatorManager.SetLocatorProvider(() => new UnityServiceLocator());
+        }
+    }
+}
