d361bd03995870ee67d701cc26242bf3c3fce07d
darrencauthon <darren@cauthon.com>
2010-08-11 21:31:11 -0500

    Cleanup.

--- src/MvcTurbine.GoogleSiteMap/Helpers/ChangeFrequencyOption.cs
+++ src/MvcTurbine.GoogleSiteMap/Helpers/ChangeFrequencyOption.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace MvcTurbine.GoogleSiteMap.Helpers
+﻿namespace MvcTurbine.GoogleSiteMap.Helpers
 {
     public enum ChangeFrequencyOption
     {
         NA,
         Always
-        
     }
 }
