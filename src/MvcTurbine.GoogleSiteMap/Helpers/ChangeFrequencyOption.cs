8c0676a82deacaeb2ff2e5601b8c1d124229b6a5
darrencauthon <darren@cauthon.com>
2010-08-11 21:13:33 -0500

    Add file missing from previous commit.

---
+++ src/MvcTurbine.GoogleSiteMap/Helpers/ChangeFrequencyOption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcTurbine.GoogleSiteMap.Helpers
+{
+    public enum ChangeFrequencyOption
+    {
+        NA,
+        Always
+        
+    }
+}
